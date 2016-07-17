39dd2e838416f8369697cabaa9d829cc7180d4f2
Udo Sauer <udo@macbook.fritz.box>
2016-07-17 17:28:52 +0200

    changed command id

--- fernsehmuell_Reverse_Play_Shuttle_(J).lua
+++ fernsehmuell_Reverse_Play_Shuttle_(J).lua
@@ -6,7 +6,7 @@ end
 
 function main()
   reaper.Undo_BeginBlock()
-  reverse_function=reaper.NamedCommandLookup("_RS50ac80b4f57c3c66bb6710c2b9ef910d0694ffe8") -- fernsehmuell_Reverse_Play_Shuttle_Background.lua
+  reverse_function=reaper.NamedCommandLookup("_RS7a8e0209506190273dfcdadc59a4183963f67b7e") -- fernsehmuell_Reverse_Play_Shuttle_Background.lua
   if is_playing_reverse()>0 then
     reaper.SetProjExtState(0, "Fernsehmuell", "Reverse_Play_Shuttle", 2) --set reverse status to 2 -> button pressed again!
   else
