0fa269cd67261ead972daa93e7b62ee0dd3fd345
Udo Sauer <fernsehmuell@googlemail.com>
2016-09-20 21:52:46 +0200

    added 4 new Funtions: step forwards and backwards 40 and 400ms (1 ore 10 videoframes @25fps)

---
+++ Mediacomposer_like_functions/fernsehmuell_Step_forwards_400ms_(2).lua
@@ -0,0 +1,9 @@
+-- @version 1.0
+-- @author Udo Sauer
+-- @changelog
+--   Initial release
+
+playstate=reaper.GetPlayState() --0 stop, 1 play, 2 pause, 4 rec possible to combine bits
+if playstate==0 or playstate==2 then
+  reaper.MoveEditCursor(0.4, 0)
+end
